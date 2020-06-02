dd1d8d96c1d673f7b6ce65349f73ecb6f757b173
Shani Armon <armonshanid+development@gmail.com>
2020-06-02 16:28:23 +0300

    Add package files

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
+
+
+# from setuptools import setup, find_packages
+
+
+# test_requirements = 
+# setup(
+#     name="pymirror",
+#     packages=find_packages(include=["mirror", "mirror.*"], exclude=["mirror.tests"])
+#     tests_require=test_requirements,
+# )
