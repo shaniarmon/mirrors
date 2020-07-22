f30fae03a50e62f7fa41699e90cd1d7f1ac423ba
Shani Armon <armonshanid+development@gmail.com>
2020-07-22 17:34:50 +0300

    Resolve filename properly for casks

--- mirror/cask.py
+++ mirror/cask.py
@@ -4,6 +4,7 @@ import sys
 import logging
 import json
 import re
+import hashlib
 
 from . import download
 from .inspect import current_module_name
@@ -11,7 +12,15 @@ from .config import load_config, get_config_value
 from .process import run_subprocess, SubprocessFailed
 
 
-def mirror_cask(cask_name, output_directory):
+def sha256(filename):
+    hash = hashlib.sha256()
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash.update(chunk)
+    return hash.hexdigest()
+
+
+def mirror_cask(cask_name, output_directory, output_file):
     log.info(f"Mirroring cask '{cask_name}'")
     formula_info = run_subprocess(["brew", "cask", "info", cask_name, "--json=v1"], capture_output=True,
                                   check=True, encoding="utf8")
@@ -20,22 +29,35 @@ def mirror_cask(cask_name, output_directory):
 
     url = formula_info[0]["url"]
     version = formula_info[0]["version"]
-    url_basename = os.path.basename(url)
-    if os.path.extsep in url_basename:
-        ext = os.path.extsep + url_basename.rsplit(os.path.extsep, 1)[1]
-    else:
-        ext = ""
 
-    filename = f"{cask_name}-{version}{ext}"
-    download_path = os.path.join(output_directory, f"{cask_name}-{version}{ext}")
+    filename = download.get_download_name(url)
+    output_file.write(f"artifact-filename: {filename}\n")
+    output_file.write(f"formula: {cask_name}.rb\n")
+    output_file.write(f"artifact-sha256: {formula_info[0]['sha256']}\n")
+
+    download_path = os.path.join(output_directory, filename)
     log.debug("Downloading ")
-    download.download_file(url, download_path)
+    already_done = False
+    if os.path.exists(download_path):
+        downloaded_hash = sha256(download_path)
+        if downloaded_hash == formula_info[0]['sha256']:
+            log.info("File already exists")
+            already_done = True
+    
+    if not already_done:
+        download.download_file(url, download_path)
+
+        downloaded_hash = sha256(download_path)
+        if downloaded_hash == formula_info[0]['sha256']:
+            log.info("Successfully verified download hash")
+        else:
+            log.error("Download hash did not match formula: %s", f"{downloaded_hash=} != {formula_info[0]['sha256']=}")
+            raise ValueError(f"Hash of formule '{cask_name}' was not successfully verified")
 
     formula = run_subprocess(["brew", "cask", "cat", cask_name],
                              capture_output=True, check=True, encoding="utf8").stdout
 
-    formula = re.sub(f"url [\"'].*[\"']", "url '{{placeholder_for_url}}/{filename}'", formula, count=1)
-
+    formula = re.sub("url [\"'].*[\"']", f"url '{{placeholder_for_url}}/{filename}'", formula, count=1)
     with open(os.path.join(output_directory, f"{cask_name}.rb"), "w") as f:
         f.write(formula)
 
@@ -51,16 +73,16 @@ def mirror_casks(config_file, output_directory):
     os.truncate(os.path.join(output_directory, "casks.status"), 0)
     for cask in casks:
         try:
-            mirror_cask(cask, cask_directory)
             with open(os.path.join(output_directory, "casks.status"), "a") as f:
+                mirror_cask(cask, cask_directory, f)
                 f.write(f"{cask}: success\n")
-        except Exception:
-            log.info(f"Could not mirror cask '{cask}'")
+        except Exception as e:
+            log.info(f"Could not mirror cask '{cask}'. Failed with error {e.__class__.__name__}{e.args}")
             log.debug("Exception when downloading cask ", exc_info=True)
             with open(os.path.join(output_directory, "casks.status"), "a") as f:
                 f.write(f"{cask}: failed\n")
-
-
+        with open(os.path.join(output_directory, "casks.status"), "a") as f:
+            f.write("---\n")
 
 
 def main(argv):
--- mirror/download.py
+++ mirror/download.py
@@ -1,15 +1,40 @@
 import requests
 from contextlib import nullcontext
+import os
+import werkzeug
+import urllib
+
+
+def get_download_name_from_request(request):
+    if 'Content-Disposition' in request.headers:
+        disposition = werkzeug.http.parse_options_header(request.headers['Content-Disposition'])[1]
+        if 'filename' in disposition:
+            return urllib.parse.unquote(disposition['filename'])
+
+    return urllib.parse.unquote(os.path.basename(request.request.path_url))
+
+
+def get_download_name(url):
+    with requests.get(url, stream=True) as request:
+        return get_download_name_from_request(request)
 
 
 def download_file(url, filename_or_obj):
-    if isinstance(filename_or_obj, str):
-        fileobj = open(filename_or_obj, 'wb')
-    else:
-        fileobj = nullcontext(filename_or_obj)
+    filename = None
+    with requests.get(url, stream=True) as request:
+        if not isinstance(filename_or_obj, str):
+            fileobj = nullcontext(filename_or_obj)
+        elif os.path.isdir(filename_or_obj):
+            filename = os.path.join(filename_or_obj, get_download_name_from_request(request))
+            fileobj = open(filename, 'wb')
+        else:
+            filename = filename_or_obj
+            fileobj = open(filename_or_obj, 'wb')
 
-    with fileobj as f:
-        with requests.get(url, stream=True) as request:
-            request.raise_for_status()
-            for chunk in request.iter_content(chunk_size=8192):
-                f.write(chunk)
+        with fileobj as f:
+            with requests.get(url, stream=True) as request:
+                request.raise_for_status()
+                for chunk in request.iter_content(chunk_size=8192):
+                    f.write(chunk)
+
+    return filename
